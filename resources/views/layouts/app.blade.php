639873eaba29d57337eb1a7d31cc8d7ee22e7b0f
MiroKoLe <miroslavkolev@gmail.com>
2022-10-07 14:23:15 +0200

    First commit; application setup

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/js/app.js')
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body class="bg-gray-200">
+
+<nav class="p-6 bg-white flex justify-between mb-6">
+    <ul class="flex items-center">
+        <li class="p-3">
+            <a href="">Home</a>
+        </li>
+        <li class="p-3">
+        <a href="">Dashboard</a>
+        </li>
+        <li class="p-3">
+        <a href="">Posts</a>
+        </li>
+    </ul>
+</nav>
+@yield('content')
+</body>
+</html>
