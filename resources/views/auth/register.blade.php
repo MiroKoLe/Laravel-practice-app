5995d963d1a498e7b1c7457f0f54eca674b8c27c
MiroKoLe <miroslavkolev@gmail.com>
2022-10-08 16:49:44 +0200

    registration layout

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+   <div class="flex justify-center">
+    <div class="w-6/12 bg-white p-6 rounded-lg">
+        <form action="{{route('register')}}" method="posts">
+            <div class="mb-4">
+                <label for="name" class="sr-only">Name</label>
+                <input type="text" name="name" id="name" placeholder="Your Name" value="">
+                <class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+            </div>
+
+            <div class="mb-4">
+                <label for="username" class="sr-only">Username</label>
+                <input type="text" name="username" id="username" placeholder="Your Username" value="">
+                <class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+            </div>
+
+            <div class="mb-4">
+                <label for="email" class="sr-only">Email</label>
+                <input type="text" name="email" id="email" placeholder="Your Email" value="">
+                <class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+            </div>
+
+            <div class="mb-4">
+                <label for="password" class="sr-only">Password</label>
+                <input type="password" name="password" id="password" placeholder="Choose a password" value="">
+                <class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+            </div>
+
+            <div class="mb-4">
+                <label for="password_confirmation" class="sr-only">Password again</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" placeholder="Repeat password" value="">
+                <class="bg-gray-100 border-2 w-full p-4 rounded-lg" value="">
+            </div>
+
+            <div>
+                <button type="submit" class="bg-blue-500" text-white px-4 py-3 rounded font-medium w-full">
+                Register</button>
+            </div>
+        </form>
+    </div>
+   </div>
+@endsection
