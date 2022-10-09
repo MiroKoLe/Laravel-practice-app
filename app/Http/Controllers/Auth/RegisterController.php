892711cc1fc0f170ad0e5990725bdcb6d9c6f7a7
MiroKoLe <miroslavkolev@gmail.com>
2022-10-09 18:42:45 +0200

    some changes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class RegisterController
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
     public function index()
     {
         return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        //validation
+        $this->validate($request, [
+            'name' =>'required|max:255',
+            'username' =>'required',
+            'email' =>'required|email|max:255',
+            'password' =>'required|confirmed'
+        ]);
+
     }
 }
